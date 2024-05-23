0135453ecb2d10ead6ba63061433d08c600e95e8
Devcaiodias <caiodiasmartins12@gmail.com>
2024-05-23 17:36:41 -0300

    Criando Arrays NumPy

---
+++ classroom6.py
@@ -0,0 +1,15 @@
+import numpy
+
+# version do Numpy
+# print(numpy.__version__)
+
+# Criando Arrays Numpy
+
+arr1 = numpy.array([10, 21, 32, 124, 443, 35, 43])
+print(arr1)
+
+# Um objeto do tipo ndarray é um recipiente multidimensional de itens do mesmo tipo e tamanho
+print(type(arr1))
+
+# verificando o formato do array
+print(arr1.shape)
