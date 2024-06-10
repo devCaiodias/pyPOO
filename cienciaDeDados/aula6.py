f6a8780e8d4ebdf477bb3a7265a917c05cae27ed
Devcaiodias <caiodiasmartins12@gmail.com>
2024-06-10 10:59:12 -0300

    Manipulação de Dados - Numpy - Operações

--- cienciaDeDados/aula6.py
+++ cienciaDeDados/aula6.py
@@ -1,2 +1,69 @@
 import numpy as np
 import pandas as pd
+
+np.random.seed(42)
+arr1 = np.array([1,2,3])
+arr2 = np.array([4,5,6])
+m1 = np.random.randint(1,10, size=(3,3))
+m2 = np.random.randint(1,10, size=(3,3))
+
+print(m1)
+print(m2)
+print()
+
+# Soma
+print(arr1 + arr2)
+print(np.add(arr1, arr2))
+
+# subtração
+
+print(arr1 - arr2)
+print(np.subtract(arr1, arr2))
+
+# Multiplicação (escalar)
+
+print(arr1 * 2)
+print(arr1 * 2.1)
+print(np.multiply(arr1, 2))
+
+# Multiplicação (element-wise)
+
+print(arr1 * arr2)
+print(np.multiply(arr1, arr2)) 
+
+# Multiplicação (produto vetorial)
+
+print(sum(arr1 * arr2))
+print(np.dot(arr1, arr2))
+
+print(np.cross(arr1, arr2))
+
+print(m1@m2)
+print(np.matmul(m1, m2))
+
+# Divisão
+
+print(arr1 / arr2)
+print(np.divide(arr1, arr2))
+
+# Exponenciação
+
+print(arr1 ** 2)
+print(arr1 ** arr2)
+print(np.power(arr1, arr2))
+
+# Modulo de vetor
+
+print(sum(arr1 * arr1) ** 0.5)
+print(np.linalg.norm(arr1))
+
+# Determinante de uma matriz
+
+print(np.linalg.det(m1))
+
+# Matriz inversa
+print(np.linalg.inv(m1))
+
+# Matriz transporta
+print(np.transpose(m1))
+print(m1.T)
